b9d6bd068f601948e630fc5ef6e736fde1939ed3
Michael Taiwo <taiwonator77@gmail.com>
2020-02-26 23:51:57 +0000

    simple todolist

---
+++ src/components/Todo.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class Todo extends Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+                <h3 style={{display:'inline-block'}}>{this.props.text}</h3>
+                <button onClick={() => this.props.deleteTodo(this.props.id)} style={{margin: '5px'}}>Delete</button>
+            </div>
+        )
+    }
+}
+
+export default Todo;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,22 +1,26 @@
 import React, {Component} from 'react';
+import Todo from '../components/Todo';
 
 class TodoList extends Component {
 
     constructor(props) {
         super(props);
-        console.log(props);
     }
 
-    listTodos(todo) {
-        return <li key={todo}>{todo}</li>
+    uniqueKey = () => {
+        return Date.now();
+    }
+
+    listTodos = (todo) => {
+        return <Todo deleteTodo={this.props.deleteTodo} text={todo.text} key={todo.key} id={todo.id} ></Todo>
     }
 
     render() {
         const todos = this.props.todos;
         const list_todos = todos.map(this.listTodos);
-        
+
         return (
-        <ul>{list_todos}</ul>
+            <ul>{list_todos}</ul>
         )
     }
 }
