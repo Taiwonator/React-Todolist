4657b7bc2fdcd367c4000e0e62d4bc8bdd9f9466
Michael Taiwo <taiwonator77@gmail.com>
2020-02-26 16:23:12 +0000

    basic start

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,16 @@
 import React, {Component} from 'react';
-import TodoList from '../components/TodoList';
 
 const x = 2;
 
 class App extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            title: 'Default Title',
-        };
-        console.log(props);
-    }
-
-    changeTitle = (title) => {
-        this.setState ({
-            title
-        });
-        console.log("Working");
-    }
-
-    handleChange = (e) => {
-        const title = e.target.value;
-        this.changeTitle(title);
-    }
-            
+    constructor() {
+        super();
+    }            
 
     render() {
             return (
-            <TodoList></TodoList>
+            <div/>
         )
     }
 }
