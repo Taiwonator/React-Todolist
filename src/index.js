c3296522de14083eaf35e8d5bf68e2ecc471aa87
Michael Taiwo <taiwonator77@gmail.com>
2020-02-27 15:25:15 +0000

    inital commi t

--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-// ReactDOM.render(<App />, document.querySelector('#app'));
-ReactDOM.render(<App class='dummy'/>, document.getElementById('app'));
+ReactDOM.render(<App/>, document.getElementById('app'));
